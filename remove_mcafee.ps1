d712739a0c7a614dc4368775720a4e0b5fa05f0e
Noah Barrett <134751923+ncbarrett90@users.noreply.github.com>
2025-06-06 08:28:54 -0500

    Delete remove_mcafee.ps1

--- remove_mcafee.ps1
+++
@@ -1,3 +0,0 @@
-# Script that removes all McAfee products using the McAfee Consumer Product Removal tool (MCPR)
-#### must run <Set-ExecutionPolicy RemoteSigned -Scope Process> to allow scripts to execute for the current process ####
-
